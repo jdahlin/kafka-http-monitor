5fcd799d30d36bf922f934b29a11c8f94e83052a
Johan Dahlin <jdahlin@gmail.com>
2023-04-05 10:10:56 +0200

    logging: Make it human readable and add test

--- src/kafka_http_monitor/tests/test_consumer.py
+++ src/kafka_http_monitor/tests/test_consumer.py
@@ -3,7 +3,10 @@ import asyncio
 import dataclasses
 import http
 import json
+import logging
 from unittest.mock import ANY, AsyncMock, MagicMock, call, patch
+
+from _pytest.logging import LogCaptureFixture
 
 from kafka_http_monitor.consumer import (
     RECORD_COLUMNS,
@@ -23,6 +26,7 @@ def test_consumer_async_main(
     create_client: MagicMock,
     kafka_options: KafkaOptions,
     url_stats: UrlStats,
+    caplog: LogCaptureFixture,
 ) -> None:
     """Test the consumer async main function."""
     connect.return_value = sql_conn = AsyncMock()
@@ -30,7 +34,21 @@ def test_consumer_async_main(
     kafka_consumer.__aiter__.return_value = [
         MagicMock(value=json.dumps(dataclasses.asdict(url_stats))),
     ]
-    asyncio.run(async_main(kafka_options, "postgresql://localhost", 0))
+    with caplog.at_level(logging.INFO):
+        asyncio.run(async_main(kafka_options, "postgresql://localhost", 0))
+    assert caplog.record_tuples == [
+        ("kafka_http_monitor.consumer", 20, "Connected to db, creating DDL"),
+        ("kafka_http_monitor.consumer", 20, "DDL created"),
+        ("kafka_http_monitor.consumer", 20, "Connected to kafka"),
+        ("kafka_http_monitor.consumer", 20, "polling messages"),
+        (
+            "kafka_http_monitor.consumer",
+            20,
+            'MESSAGE! {"url": "http://localhost", "method": "GET", '
+            '"response_time_in_milliseconds": 1234, "response_status_code": 200, '
+            '"regex": null, "response_matched_regex": false} 1',
+        ),
+    ]
 
     assert sql_conn.mock_calls == [
         call.execute(SQL_CREATE_TABLES_AND_VIEWS),
